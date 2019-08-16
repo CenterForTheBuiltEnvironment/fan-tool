7f77985293eed545f1f9d313c3537e96f88d3051
praftery <p.raftery@gmail.com>
2019-08-16 16:15:26 -0700

    minor bug fix and improvements
    link solution selection in table to correct solution for display
    clear floor plan fan layout when no solution
    scale floor plan to canvas, added solution info on graphic

--- main.js
+++ main.js
@@ -35,6 +35,7 @@ room = [];
 candidateFans = []
 solutions = [];
 isSIunits = true;
+scale = 1;
 
 // // get parameters from the url
 // var url = new URL(document.URL);
@@ -60,274 +61,286 @@ isSIunits = true;
 // }
 
 $(document).ready(function() {
-  $( "#accordion" ).accordion({
-    collapsible: true,
-    heightStyle: "content"
-  });
-
-  $( "#slider-dimless" ).slider({
-    range: true,
-    min: 0.15,
-    max: 0.60,
-    values: [ lims.dimensionlessDiameter.min, lims.dimensionlessDiameter.max ],
-    step: 0.01,
-    slide: function( event, ui ) {
-      $( "#dimless" ).val( ui.values[ 0 ] + " - "  + ui.values[ 1 ]);
-      lims.dimensionlessDiameter.max = ui.values[ 1 ];
-      lims.dimensionlessDiameter.min = ui.values[ 0 ];
-    }
-  });
-  $( "#dimless" ).val( $( "#slider-dimless" ).slider( "values", 0 ) +
-  " - " + $( "#slider-dimless" ).slider( "values", 1 ));
-
-  $( "#slider-cellSize" ).slider({
-    range: true,
-    min: 4.5,
-    max: 15.24,
-    values: [ lims.cellSize.min, lims.cellSize.max ],
-    step: 0.1,
-    slide: function( event, ui ) {
-
-      $( "#cellSize" ).val( ui.values[ 0 ] + " - "  + ui.values[ 1 ]);
-      lims.cellSize.max = ui.values[ 1 ];
-      lims.cellSize.min = ui.values[ 0 ];
-    }
-  });
-
-  $( "#cellSize" ).val( $( "#slider-cellSize" ).slider( "values", 0 ) +
-  " - " + $( "#slider-cellSize" ).slider( "values", 1 ));
-
-  $( "#slider-aspectRatio-min" ).slider({
-    range: "min",
-    value: 1.25,
-    min: 1,
-    max: 1.5,
-    step: 0.01,
-    slide: function( event, ui ) {
-      $( "#aspectRatio" ).val( "" + ui.value );
-    }
-  });
-  $( "#aspectRatio" ).val( "" + $( "#slider-aspectRatio-min" ).slider( "value" ) );
-
-  $( "#slider-len-min" ).slider({
-    range: "min",
-    value: 25,
-    min: 4.5,
-    max: 100,
-    step: 0.1,
-    slide: function( event, ui ) {
-      updateSliderDisplays();
-    }
-  });
-
-  $( "#slider-wid-min" ).slider({
-    range: "min",
-    value: 20,
-    min: 4.5,
-    max: 100,
-    step: 0.1,
-    slide: function( event, ui ) {
-      $( "#wid" ).val( "" + ui.value );
-    }
-  });
-  $( "#wid" ).val( "" + $( "#slider-wid-min" ).slider( "value" ) );
-
-  $( "#slider-hei-min" ).slider({
-    range: "min",
-    value: 3,
-    min: 2.7,
-    max: 4.5,
-    step: 0.1,
-    slide: function( event, ui ) {
-      $( "#hei" ).val( "" + ui.value );
-    }
-  });
-  $( "#hei" ).val( "" + $( "#slider-hei-min" ).slider( "value" ) );
-
-
-  $( ":radio" ).checkboxradio({
-    icon: false
-  });
-
-  var tblFans = $('#fans').DataTable( {
-    data: [
-      ['TypeA', 1.2192,   2.611757, true],
-      ['TypeB', 1.319784, 2.258268, true],
-      ['TypeC', 1.524,    3.765196, true],
-      ['TypeD', 1.524,    3.826077, true],
-      ['TypeE', 2.1336,   7.734745, true],
-      ['TypeF', 2.4384,   13.80304, false],
-      ['TypeG', 2.4384,   16.57101, false],
-      ['TypeH', 3.048,    20.91151, false],
-      ['TypeI', 4.2672,   25.30817, false]
-    ],
-    destroy: true,
-    scrollY: "200px",
-    scrollCollapse: true,
-    paging: false,
-    searching: false,
-    info: false,
-    columnDefs: [{
-      targets: [1,2],
-      render: $.fn.dataTable.render.number(',', '.', 2)
-    }],
-    columns: [
-      { title: "Name" },
-      { title: "D (m)" },
-      { title: "Q (m³/s)" },
-      { title: "UL507" }
-    ]
-  } );
-
-  // select/deselect rows when clicked
-  $('#fans tbody').on( 'click', 'tr', function () {
-    $(this).toggleClass('selected');
-  } );
-
-  // update fans object and calculate solutions when a fan is selected/deseleccted
-  $('#fans tbody').on( 'click', function () {
-    candidateFans =[]
-    tblFans.rows('.selected').data().each( function(row,index) {
-      candidateFans.push(new Fan(...row))
-    });
-    updateSolutions();
-  } );
-
-  // add a fan using a jQuery modal form to input new fan on button click
-  $('#addFan').on( 'click', function () {
-    // replace with jQuery modal form to input new fan
-    tblFans.row.add( [
-      "Ex",
-      2,
-      9.9,
-      true,
-    ] ).draw( false );
-
-  } );
-
-  var tblSln = $('#solutions').DataTable( {
-    data: [],
-    destroy: true,
-    paging: true,
-    scrollY: true,
-    searching: false,
-    info: true,
-    lengthChange: false,
-    pageLength: 10,
-    columnDefs: [{
-      targets: [2,3,4,5],
-      render: $.fn.dataTable.render.number(',', '.', 2)
-    }],
-    columns: [
-      { title: "Fan type" },
-      { title: "# fans" },
-      { title: "Min airspeed (m/s)" },
-      { title: "Avg airspeed (m/s)" },
-      { title: "Max airspeed (m/s)" },
-      { title: "Unifomity" }
-    ]
-  } );
-
-  $('#solutions tbody').on( 'click', 'tr', function () {
-    if ( $(this).hasClass('selected') ) {
-      $(this).removeClass('selected');
-    }
-    else {
-      tblSln.$('tr.selected').removeClass('selected');
-      $(this).addClass('selected');
-      drawFans(this.rowIndex-1);
-    }
-  } );
-
-  $('.ui-slider').mouseup(function () {
-    updateSolutions();
-  });
-
-  $('.ui-spinner-button').click(function() { $(this).siblings('input').change(); });
-
-  $(':input').change(function () {
-    updateSolutions();
-  });
-
-  // recalc the solutions, clear the floor plan, update the solutions table
-  function updateSolutions() {
-    calcSolutions();
-    drawRoom();
-    updateSlnTable();
-    updateSliderDisplays();
-  };
-
-  function updateSlnTable(){
-    tblSln.clear();
-    tblData = [];
-    for (i of solutions){
-      tblData.push([
-        i.fan.type,
-        i.layout.numFans(),
-        (isSIunits) ? i.airspeeds()[0] : i.airspeeds()[0] * 196.85,
-        (isSIunits) ? i.airspeeds()[1] : i.airspeeds()[1] * 196.85,
-        (isSIunits) ? i.airspeeds()[2] : i.airspeeds()[2] * 196.85,
-        i.airspeeds()[3]
-      ]);
-    }
-    tblSln.rows.add(tblData);
-    tblSln.draw();
-    // select first solution if any solutions exist
-    if (solutions.length >0 ) $('#solutions tbody tr:eq(0)').click();
-  };
-
-  // if someone wants to change to units
-  $("[name='units']").on( 'click', function () {
-    changeUnits();
-  });
-
-  function updateSliderDisplays(){
-    // TODO: generalize this for any slider, grouped by unit type
-    if (isSIunits) {
-      $( "#len" ).val( $( "#slider-len-min" ).slider( "value" ) + " m" );
-    } else {
-      $( "#len" ).val( ($( "#slider-len-min" ).slider( "value" )*3.2808).toFixed(1) + " ft" );
-    }
-  }
-
-  function changeUnits () {
-    isSIunits  = $("[name='units']")[0].checked
-    // update table column names
-
-    if (isSIunits) {
-      $(tblFans.column(1).header()).text('D (m)');
-      $(tblFans.column(2).header()).text('Q (m3/s)');
-      $(tblSln.column(2).header()).text('Min airspeed (m/s)');
-      $(tblSln.column(3).header()).text('Avg airspeed (m/s)');
-      $(tblSln.column(4).header()).text('Max airspeed (m/s)');
-      // update the data in each column
-      tblFans.rows().every( function ( rowIdx, tableLoop, rowLoop ) {
-        var data = this.data();
-        data[1] /= 3.2808;
-        data[2] /= 2118.88;
-        this.data(data);
-      });
-    } else {
-      $(tblFans.column(1).header()).text('D (ft)');
-      $(tblFans.column(2).header()).text('Q (cfm)');
-      $(tblSln.column(2).header()).text('Min airspeed (fpm)');
-      $(tblSln.column(3).header()).text('Avg airspeed (fpm)');
-      $(tblSln.column(4).header()).text('Max airspeed (fpm)');
-      // update the data in each column
-      tblFans.rows().every( function ( rowIdx, tableLoop, rowLoop ) {
-        var data = this.data();
-        data[1] *= 3.2808;
-        data[2] *= 2118.88;
-        this.data(data);
-      });
-    }
-    updateSliderDisplays();
-  };
-
   // calculate solutions for the initial condition
   updateSolutions();
+});
+
+$( "#accordion" ).accordion({
+  collapsible: true,
+  heightStyle: "content"
+});
+
+$( "#slider-dimless" ).slider({
+  range: true,
+  min: 0.15,
+  max: 0.60,
+  values: [ lims.dimensionlessDiameter.min, lims.dimensionlessDiameter.max ],
+  step: 0.01,
+  slide: function( event, ui ) {
+    $( "#dimless" ).val( ui.values[ 0 ] + " - "  + ui.values[ 1 ]);
+    lims.dimensionlessDiameter.max = ui.values[ 1 ];
+    lims.dimensionlessDiameter.min = ui.values[ 0 ];
+  }
+});
+$( "#dimless" ).val( $( "#slider-dimless" ).slider( "values", 0 ) +
+" - " + $( "#slider-dimless" ).slider( "values", 1 ));
+
+$( "#slider-cellSize" ).slider({
+  range: true,
+  min: 4.5,
+  max: 15.24,
+  values: [ lims.cellSize.min, lims.cellSize.max ],
+  step: 0.1,
+  slide: function( event, ui ) {
+
+    $( "#cellSize" ).val( ui.values[ 0 ] + " - "  + ui.values[ 1 ]);
+    lims.cellSize.max = ui.values[ 1 ];
+    lims.cellSize.min = ui.values[ 0 ];
+  }
+});
+
+$( "#cellSize" ).val( $( "#slider-cellSize" ).slider( "values", 0 ) +
+" - " + $( "#slider-cellSize" ).slider( "values", 1 ));
+
+$( "#slider-aspectRatio-min" ).slider({
+  range: "min",
+  value: 1.25,
+  min: 1,
+  max: 1.5,
+  step: 0.01,
+  slide: function( event, ui ) {
+    $( "#aspectRatio" ).val( "" + ui.value );
+  }
+});
+$( "#aspectRatio" ).val( "" + $( "#slider-aspectRatio-min" ).slider( "value" ) );
+
+$( "#slider-len-min" ).slider({
+  range: "min",
+  value: 25,
+  min: 4.5,
+  max: 100,
+  step: 0.1,
+  slide: function( event, ui ) {
+    updateSliderDisplays();
+  }
+});
+
+$( "#slider-wid-min" ).slider({
+  range: "min",
+  value: 20,
+  min: 4.5,
+  max: 100,
+  step: 0.1,
+  slide: function( event, ui ) {
+    $( "#wid" ).val( "" + ui.value );
+  }
+});
+$( "#wid" ).val( "" + $( "#slider-wid-min" ).slider( "value" ) );
+
+$( "#slider-hei-min" ).slider({
+  range: "min",
+  value: 3,
+  min: 2.7,
+  max: 4.5,
+  step: 0.1,
+  slide: function( event, ui ) {
+    $( "#hei" ).val( "" + ui.value );
+  }
+});
+$( "#hei" ).val( "" + $( "#slider-hei-min" ).slider( "value" ) );
+
+
+$( ":radio" ).checkboxradio({
+  icon: false
+});
+
+var tblFans = $('#fans').DataTable( {
+  data: [
+    ['TypeA', 1.2192,   2.611757, true],
+    ['TypeB', 1.319784, 2.258268, true],
+    ['TypeC', 1.524,    3.765196, true],
+    ['TypeD', 1.524,    3.826077, true],
+    ['TypeE', 2.1336,   7.734745, true],
+    ['TypeF', 2.4384,   13.80304, false],
+    ['TypeG', 2.4384,   16.57101, false],
+    ['TypeH', 3.048,    20.91151, false],
+    ['TypeI', 4.2672,   25.30817, false]
+  ],
+  destroy: true,
+  scrollY: "200px",
+  scrollCollapse: true,
+  paging: false,
+  searching: false,
+  info: false,
+  columnDefs: [{
+    targets: [1,2],
+    render: $.fn.dataTable.render.number(',', '.', 2)
+  }],
+  columns: [
+    { title: "Name" },
+    { title: "D (m)" },
+    { title: "Q (m³/s)" },
+    { title: "UL507" }
+  ]
+} );
+
+// select/deselect rows when clicked
+$('#fans tbody').on( 'click', 'tr', function () {
+  $(this).toggleClass('selected');
+} );
+
+// update fans object and calculate solutions when a fan is selected/deseleccted
+$('#fans tbody').on( 'click', function () {
+  candidateFans =[]
+  tblFans.rows('.selected').data().each( function(row,index) {
+    candidateFans.push(new Fan(...row))
+  });
+  updateSolutions();
+} );
+
+// add a fan using a jQuery modal form to input new fan on button click
+$('#addFan').on( 'click', function () {
+  // replace with jQuery modal form to input new fan
+  tblFans.row.add( [
+    "Ex",
+    2,
+    9.9,
+    true,
+  ] ).draw( false );
 
 } );
+
+var tblSln = $('#solutions').DataTable( {
+  data: [],
+  destroy: true,
+  paging: true,
+  scrollY: true,
+  searching: false,
+  info: true,
+  lengthChange: false,
+  pageLength: 10,
+  columnDefs: [{
+    targets: [2,3,4,5],
+    render: $.fn.dataTable.render.number(',', '.', 2)
+  }],
+  columns: [
+    { title: "Fan type" },
+    { title: "# fans" },
+    { title: "Min airspeed (m/s)" },
+    { title: "Avg airspeed (m/s)" },
+    { title: "Max airspeed (m/s)" },
+    { title: "Unifomity" }
+  ]
+} );
+
+$('#solutions tbody').on( 'click', 'tr', function () {
+  if ( $(this).hasClass('selected') ) {
+    $(this).removeClass('selected');
+  }
+  else {
+    tblSln.$('tr.selected').removeClass('selected');
+    $(this).addClass('selected');
+  }
+} );
+
+// update selected solution
+$('#solutions tbody').on( 'click', function () {
+  if (tblSln.rows('.selected').data().length > 0 ){
+    f = tblSln.rows('.selected').data()[0][0];
+    n = tblSln.rows('.selected').data()[0][1];
+    for (sln of solutions){
+      // TODO - resolve issue if two matching slns (same type, 7 x 8 vs 8x7 fans)
+      if (sln.fan.type == f && sln.layout.numFans() == n) drawFans(sln);
+    };
+  };
+} );
+
+$('.ui-slider').mouseup(function () {
+  updateSolutions();
+});
+
+$('.ui-spinner-button').click(function() { $(this).siblings('input').change(); });
+
+$(':input').change(function () {
+  updateSolutions();
+});
+
+// recalc the solutions, clear the floor plan, update the solutions table
+function updateSolutions() {
+  calcSolutions();
+  drawRoom();
+  updateSlnTable();
+  updateSliderDisplays();
+};
+
+function updateSlnTable(){
+  tblSln.clear();
+  tblData = [];
+  for (i of solutions){
+    tblData.push([
+      i.fan.type,
+      i.layout.numFans(),
+      (isSIunits) ? i.airspeeds()[0] : i.airspeeds()[0] * 196.85,
+      (isSIunits) ? i.airspeeds()[1] : i.airspeeds()[1] * 196.85,
+      (isSIunits) ? i.airspeeds()[2] : i.airspeeds()[2] * 196.85,
+      i.airspeeds()[3]
+    ]);
+  }
+  tblSln.rows.add(tblData);
+  tblSln.draw();
+  // select first solution if any solutions exist
+  if (solutions.length >0 ) $('#solutions tbody tr:eq(0)').click();
+};
+
+// if someone wants to change to units
+$("[name='units']").on( 'click', function () {
+  changeUnits();
+});
+
+function updateSliderDisplays(){
+  // TODO: general
+  if (isSIunits) {
+    $( "#len" ).val( $( "#slider-len-min" ).slider( "value" ) + " m" );
+  } else {
+    $( "#len" ).val( ($( "#slider-len-min" ).slider( "value" )*3.2808).toFixed(1) + " ft" );
+  }
+}
+
+function changeUnits () {
+  isSIunits  = $("[name='units']")[0].checked
+  // update table column names
+
+  if (isSIunits) {
+    $(tblFans.column(1).header()).text('D (m)');
+    $(tblFans.column(2).header()).text('Q (m3/s)');
+    $(tblSln.column(2).header()).text('Min airspeed (m/s)');
+    $(tblSln.column(3).header()).text('Avg airspeed (m/s)');
+    $(tblSln.column(4).header()).text('Max airspeed (m/s)');
+    // update the data in each column
+    tblFans.rows().every( function ( rowIdx, tableLoop, rowLoop ) {
+      var data = this.data();
+      data[1] /= 3.2808;
+      data[2] /= 2118.88;
+      this.data(data);
+    });
+  } else {
+    $(tblFans.column(1).header()).text('D (ft)');
+    $(tblFans.column(2).header()).text('Q (cfm)');
+    $(tblSln.column(2).header()).text('Min airspeed (fpm)');
+    $(tblSln.column(3).header()).text('Avg airspeed (fpm)');
+    $(tblSln.column(4).header()).text('Max airspeed (fpm)');
+    // update the data in each column
+    tblFans.rows().every( function ( rowIdx, tableLoop, rowLoop ) {
+      var data = this.data();
+      data[1] *= 3.2808;
+      data[2] *= 2118.88;
+      this.data(data);
+    });
+  }
+  updateSliderDisplays();
+};
+
+
 
 
 
@@ -455,9 +468,13 @@ function drawRoom() {
     var ctx = canvas.getContext('2d')
     //clear plan after each solution selection
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    scale = Math.min(
+      (canvas.width-20)/room.sizeX,
+      (canvas.height-20)/room.sizeY
+    );
     // draw the room in plan
     ctx.beginPath();
-    ctx.rect(10,10, room.sizeX*5,room.sizeY*5);
+    ctx.rect(10,10, room.sizeX*scale,room.sizeY*scale);
     ctx.stroke();
   } else {
     alert("Your browser doesn't support HTML 5 Canvas");
@@ -465,7 +482,7 @@ function drawRoom() {
 }
 
 
-function drawFans(slnID) {
+function drawFans(sln) {
   //TODO: review pros/cons of doing this with SVG instead of canvas
   var canvas = document.getElementById('canv');
   // Execute only if canvas is supported
@@ -473,7 +490,17 @@ function drawFans(slnID) {
     var ctx = canvas.getContext('2d')
     //clear plan after each solution selection
     drawRoom();
-    sln = solutions[slnID];
+    // display basic information about the layout
+    ctx.font = '14px serif';
+    ctx.fillText(sln.layout.numFans() + " " + sln.fan.type + " fans", 14, 28);
+
+    // add diameter
+    ctx.fillText(
+      sln.fan.diameter.toFixed(1) + " m",
+      10 + scale*(0.5*(sln.layout.cellSizeX - 0.5 * sln.fan.diameter)),
+      10 + scale*(0.5*(sln.layout.cellSizeY))
+    );
+
     //draw 'fans'
     i = 0;
     j = 0;
@@ -481,12 +508,13 @@ function drawFans(slnID) {
       for (j = 0; j < sln.layout.numFansY; j++){
         ctx.beginPath();
         ctx.arc(
-          10 + 5*(i + 0.5)*sln.layout.cellSizeX,
-          10 + 5*(j + 0.5)*sln.layout.cellSizeY,
-          sln.fan.diameter*5/2, 0, 2 * Math.PI);
+          10 + scale*(i + 0.5)*sln.layout.cellSizeX,
+          10 + scale*(j + 0.5)*sln.layout.cellSizeY,
+          sln.fan.diameter*scale/2, 0, 2 * Math.PI);
           ctx.stroke();
         }
       }
+
     } else {
       alert("Your browser doesn't support HTML 5 Canvas");
     }
